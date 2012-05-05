793219e8ce16cfd35e8b0ce906b31cc20106a11b
Xaris Tsimpouris <xaris.tsimpouris@gmail.com>
2012-05-05 12:56:43 +0300

    prompt function prototype, minor fixes

--- home_alarm.ino
+++ home_alarm.ino
@@ -45,35 +45,55 @@
 #define PIN_ZONE_1      A0
 #define ALL_ZONES        6
 
+// Alarm arm types
+#define ARM_ALL          0
+#define ARM_1            1
+#define ARM_2            2
+#define ARM_3            3
+
 // Other defines
-#define PASSWORD_LENGTH  4
-#define FACTORY_PASSWORD {0, 0, 0, 0} // Should be of PASSWORD_LENGTH 
+#define PASSWORD_LENGTH   4
+#define FACTORY_PASSWORD  {0, 0, 0, 0} // Should be of PASSWORD_LENGTH 
+#define CELL_PHONE_LENGTH 10
 
 // Eeprom adresses
 // ..No other way(?) than hardcoded addresses
 // ..order matters, as each one is calculated from the previous one
 #define ADDR_START              0
 #define ADDR_PASSWORD           ADDR_START + 1
-#define ADDR_PANIC_PASSWORD     ADDR_PASSWORD + PASSWORD_LENGTH * sizeof(byte)
-
+#define ADDR_PANIC_PASSWORD     ADDR_PASSWORD + PASSWORD_LENGTH * sizeof(char)
+#define CELL_PHONE_1            ADDR_PANIC_PASSWORD + PASSWORD_LENGTH * sizeof(char)  // In case wirh AT commands we can communicate with a cell phone
+#define CELL_PHONE_2            CELL_PHONE_1 + CELL_PHONE_LENGTH * sizeof(char)
+#define ADDR_ARM_ALL_MASK       CELL_PHONE_2 + CELL_PHONE_LENGTH * sizeof(char)       // With one byte we can store zone masks
+#define ADDR_ARM_1_MASK         ADDR_ARM_ALL_MASK + sizeof(byte)                      // ..assuming that zones won't be more than 8
+#define ADDR_ARM_2_MASK         ADDR_ARM_ARM_1_MASK + sizeof(byte)                    // ..and store each zone mask to one bit
+#define ADDR_ARM_3_MASK         ADDR_ARM_ARM_2_MASK + sizeof(byte)
+#define ADDR_ZONES_NC_VOLT      ADDR_ARM_ARM_3_MASK + sizeof(byte)                    // Starting point, helper define for use in for-loops
+#define ADDR_ZONE_1_NC_VOLT     ADDR_ZONES_NC_VOLT
+#define ADDR_ZONE_2_NC_VOLT     ADDR_ZONE_1_NC_VOLT + sizeof(int)
+#define ADDR_ZONE_3_NC_VOLT     ADDR_ZONE_2_NC_VOLT + sizeof(int)
+#define ADDR_ZONE_4_NC_VOLT     ADDR_ZONE_3_NC_VOLT + sizeof(int)
+#define ADDR_ZONE_5_NC_VOLT     ADDR_ZONE_4_NC_VOLT + sizeof(int)
+#define ADDR_ZONE_6_NC_VOLT     ADDR_ZONE_5_NC_VOLT + sizeof(int)
 
 // Function prototypes
 // Init all nececary libraries or external modules
 void init_libraries();
 // Check given password to eeprom stored one
-boolean check_password(byte check_password[PASSWORD_LENGTH], boolean panic_password);
+boolean check_password(char check_password[PASSWORD_LENGTH], boolean panic_password);
 // Reads zones information from eeprom to memory
 void init_zones();
 // Reprogram the whole system (pass, zones, ..)
 void reprogram_system();
 // Test system, by opening and closing everything
 void test_system();
+// Prompt user with a message, show keys presses and a description
+char prompt(char prompt_messafe[], boolean mask, char descr_message[], char term1, char term2, char message_pressed[], int max_keys, int timeout);
+// Show message
+// void message();
 
 byte password[ PASSWORD_LENGTH ];
 byte password_panic[ PASSWORD_LENGTH ];
-
-
-
 
 int LED = 11;
 int ALARM = 10;
@@ -133,7 +153,7 @@ void init_libraries() {
 }
 
 // Check given password to eeprom stored one
-boolean check_password(byte check_password[PASSWORD_LENGTH], boolean panic_password) {
+boolean check_password(char check_password[PASSWORD_LENGTH], boolean panic_password) {
   byte value;
   int start_addr;
   
@@ -145,7 +165,7 @@ boolean check_password(byte check_password[PASSWORD_LENGTH], boolean panic_passw
   }
   for(int pos = 0; pos < PASSWORD_LENGTH; pos++) {
     value = EEPROM.read(start_addr + pos);
-    if (check_password[pos] != value) {
+    if (check_password[pos] != (char)value) {
       return false;
     }
   }
@@ -159,15 +179,36 @@ void init_zones() {
 
 // Reprogram the whole system (pass, zones, ..)
 void reprogram_system() {
+  // Clean eeprom with zeros (Check EEPROM example 'eeprom_clear')
+  
+  // Set password to factory reset
+  
   // Ask new password
   // ..save new password
   
   // Ask new panic password
   // .. save new panic password
   
+  // Ask user for cell phone 1
+  // Ask user for cell phone 2
+  
+  // Ask user how many zones exist
+  
   // Force user to close all doors and windows
   // .. read all analog inputs
   // .. save all inputs as NC values for all zones
+  
+  // We assume ARM_ALL is for all doors, windows closed
+  // For ARM_1, we ask user to press number from 1 to ALL_ZONES
+  // .. and we toggle screen number to show which zones will be checked
+  // .. for example screen shows   '1.2.3.4.5.6'
+  // .. user press 4, screen gets  '1.2.3.-.5.6'
+  // .. user press 6, screen gets  '1.2.3.-.5.-'
+  // .. user press 4, screen gets  '1.2.3.4.5.-'
+  // user press '*', we lock 12345 zones and ignore zone 6
+  
+  // Do the same for ARM_2, and ARM_3
+  
   
   // Message: System is ready
 }
@@ -176,9 +217,28 @@ void reprogram_system() {
 void test_system() {
   // Check all zones
     // If a zone is open
-    // .. beep
+    // .. beep (keypad beeper, or lcd message - not home siren alarm)
     
     // Read keypress from keypad
     // .. Stop until '*' is pressed
+}
+
+// Prompt user with a message, show keys presses and a description
+// char prompt_messafe[]  : A null terminating pointer to the text we want to prompt the user
+// boolean mask           : true, if we don't want to print users input (for example password)
+// char descr_message[]   : A null terminating pointer to the description to show, for example "Press * to cancel or # to accept"
+// char term1             : If this key is pressed, funcion will stop and return term1 value
+// char term2             : If this key is pressed, funcion will stop and return term2 value
+// char message_pressed[] : Whatever user inputs, it will be stored here
+// int max_keys           : We won't accept more than max_keys as we may step into other variables in memory
+// int timeout            : If we have timeout, we stop and return 0 (it's an acceptable character)
+
+// Example 1, password input: (assume all variables already exist)
+// prompt("Please give password\0", true, "Press # to accept or * to cancel", '#', '*', return_value, PASSWORD_LENGTH, 30);
+// Example 2, accept to test: (assume all variables already exist, here 'return_value' is reduntunt)
+// prompt("Close all doors and windows\0", false, "Press # to test system or * to cancel test", '#', '*', return_value, 0, 30);
+
+char prompt(char prompt_messafe[], boolean mask, char descr_message[], char term1, char term2, char message_pressed[], int max_keys, int timeout) {
+  
 }
 
